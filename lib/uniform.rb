2d46a1df8afa2c4e5d62346cf6572ec5d833c3ef
Ben Ehmke <benehmke@gmail.com>
2019-08-30 14:07:57 -0500

    add asset path configuration for condenser

--- lib/uniform.rb
+++ lib/uniform.rb
@@ -1,9 +1,17 @@
 if defined?(::Rails)
   class UniformUi
     class Engine < ::Rails::Engine
-
-      if Sprockets::VERSION.start_with?('4')
-        Engine.config.assets.precompile += %w(uniform/manifest.js)
+      
+      if defined?(::Sprockets)
+        if Sprockets::VERSION.start_with?('4')
+          Engine.config.assets.precompile << 'uniform/manifest.js'
+        end
+      elsif defined?(::Condenser)
+        assets_dir = File.expand_path("../lib/assets", __dir__)
+        Dir.each_child(assets_dir) do |child|
+          child = File.join(assets_dir, child)
+          Engine.config.assets.path << child if File.directory?(child)
+        end
       end
       
     end
